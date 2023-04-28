ad93176958675b1f01fea4a7c9459815e4210e80
Anton Franzluebbers <anton@uga.edu>
2023-04-28 17:04:02 -0400

    no apps, but some things working

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,32 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
+extern crate directories;
+use std::{fs, path::Path};
+
+use directories::BaseDirs;
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![open_installs_folder])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
+
+#[tauri::command]
+fn open_installs_folder() {
+    if let Some(dirs) = BaseDirs::new() {
+        println!("{:?}", dirs.data_dir());
+        let path = Path::new(dirs.data_dir())
+            .join("VELAppsHub")
+            .join("Installs");
+        match fs::create_dir_all(path.clone()) {
+            Ok(()) => open::that(path).unwrap(),
+            Err(e) => println!("{:?}", e),
+        }
+    }
+}
+
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,301 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { invoke } from '@tauri-apps/api/tauri';
+	import { SettingsManager } from 'tauri-settings';
+	import type { SettingsSchema } from '../lib/types';
+
+	let accessCode = '';
+	let appsData;
+	const settingsManager = new SettingsManager<SettingsSchema>(
+		{
+			accessCode: ''
+		},
+		{
+			// options
+			fileName: 'customization-settings'
+		}
+	);
+
+	// checks whether the settings file exists and creates it if not
+	// loads the settings if it exists
+	settingsManager.initialize().then(async () => {
+		// settingsManager.setCache('accessCode', '');
+		// at a later time
+		await settingsManager.syncCache();
+		accessCode = await settingsManager.get('accessCode');
+	});
+
+	function refreshApps() {
+		fetch(`https://vel.engr.uga.edu/apps/VELAppsHub/get_apps.php?access_code=${accessCode}`)
+			.then((r) => r.json())
+			.then((r) => {
+				console.log(r);
+				appsData = r;
+			});
+	}
+</script>
+
+<div class="flex_content">
+	<div class="left_sidebar">
+		<img id="logo_img" src="/img/vel_logo_white.png" alt="vel logo" />
+		<h1>VEL Apps Hub</h1>
+		<p>
+			Install and update apps made in the Virtual Experience Laboratory at the University of Georgia
+		</p>
+		<div class="access_code_box">
+			<h3>Access Code</h3>
+			<p>Enter an access code to make apps available to download.</p>
+			<form id="access_code_form">
+				<input id="access_code_input" type="text" spellcheck="false" bind:value={accessCode} />
+				<button
+					on:click={() => {
+						console.log(accessCode);
+						settingsManager.set('accessCode', accessCode);
+						refreshApps();
+					}}>Refresh</button
+				>
+			</form>
+		</div>
+
+		<button
+			style="margin-top:2em;"
+			on:click={() => {
+				console.log('open_installs');
+				invoke('open_installs_folder', { name: 'test' });
+			}}>Open Install Folder</button
+		>
+
+		<p id="version" />
+	</div>
+	<div class="not_left_sidebar">
+		<div id="flex_apps" />
+	</div>
+</div>
+
+<style lang="scss">
+	.hide {
+		display: none;
+	}
+
+	.flex_content {
+		display: flex;
+		position: absolute;
+		height: 100%;
+		overflow: hidden;
+	}
+
+	.left_sidebar {
+		max-width: 20em;
+		min-width: 15em;
+		padding: 1em;
+		border-right: 0.1em solid black;
+		margin-right: 2em;
+	}
+
+	.left_sidebar h1 {
+		color: #fff;
+	}
+
+	.left_sidebar h3 {
+		text-align: left;
+		margin-top: 2em;
+		color: #fff;
+	}
+
+	.left_sidebar p {
+		text-align: left;
+	}
+
+	#logo_img {
+		margin: 1em;
+		width: 100%;
+		max-width: 10em;
+	}
+
+	.not_left_sidebar {
+		/* box-shadow: black 0 0 1em; */
+	}
+
+	#access_code_input {
+		width: 8em;
+	}
+
+	form input {
+		font-size: 1em;
+	}
+
+	form input[type='text'] {
+		padding: 0.4em;
+		background-color: #252525;
+		color: #ccc;
+		border: 0.1em solid #000;
+	}
+
+	form input[type='text']:hover {
+		background-color: #303030;
+	}
+
+	form input[type='text']:focus {
+		border: 0.1em solid #888;
+		background-color: #333333;
+		outline: none;
+	}
+
+	form input[type='submit'],
+	button {
+		padding: 0.4em 0.6em 0.4em 0.6em;
+		color: #ccc;
+		background-color: #222;
+		border: 0.1em solid black;
+		cursor: pointer;
+		font-size: 1em;
+	}
+
+	form input[type='submit']:hover,
+	button:hover {
+		background-color: #522;
+		border: 0.1em solid #a22;
+	}
+
+	form input[type='submit']:focus,
+	button:focus {
+		outline: none;
+	}
+
+	form input[type='submit']:active,
+	button:active {
+		background-color: #444;
+		outline: none;
+	}
+
+	#flex_apps {
+		display: flex;
+		flex-wrap: wrap;
+		height: 100%;
+		overflow-y: auto;
+		align-items: baseline;
+	}
+
+	#flex_apps .app_box {
+		min-width: 10em;
+		max-width: 15em;
+		width: 15em;
+		background-color: #282828;
+		box-shadow: 0 0 0.2em black;
+		margin: 1em;
+		padding: 1em;
+		flex-grow: 1;
+		flex-shrink: 1;
+		overflow: hidden;
+		border: 0.1em solid #0000;
+	}
+
+	#flex_apps .app_box:hover {
+		background-color: #303030;
+		border: 0.1em solid #922;
+	}
+
+	#flex_apps .app_box:hover .thumbnail {
+		transform: scale(1.01);
+	}
+
+	#flex_apps h3 {
+		margin-top: 0.2em;
+	}
+
+	#flex_apps .thumbnail {
+		width: 100%;
+	}
+
+	progress {
+		width: -webkit-fill-available;
+		border-radius: 0;
+		margin: 0.2em;
+	}
+
+	progress::-webkit-progress-bar {
+		background: #222;
+		border: 0.1em solid #000;
+	}
+
+	progress::-webkit-progress-value {
+		background: #522;
+		border: 0.1em solid #a22;
+	}
+
+	#flex_apps progress[value='0'] {
+		display: none;
+	}
+
+	#flex_apps ul {
+		list-style-type: none;
+		margin: 0;
+		padding: 0;
+	}
+
+	#flex_apps li {
+		margin: 0.2em;
+	}
+
+	#flex_apps a {
+		padding: 0.3em;
+		display: block;
+		text-decoration: none;
+		color: #ccc;
+		background-color: #222;
+		border: 0.1em solid black;
+	}
+
+	#flex_apps a:hover {
+		background-color: #522;
+		border: 0.1em solid #a22;
+	}
+
+	#notification {
+		position: fixed;
+		bottom: 20px;
+		left: 20px;
+		width: 200px;
+		padding: 20px;
+		border-radius: 5px;
+		background-color: #464646;
+		box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+		z-index: 1;
+	}
+
+	.hidden {
+		display: none;
+	}
+
+	#modal {
+		position: fixed;
+		left: 50%;
+		top: 50%;
+		margin-left: -10em;
+		margin-top: -3em;
+		width: 20em;
+		padding: 1em;
+		background-color: #444;
+		border: 0.1em solid black;
+		box-shadow: 0 0.1em 0.2em 0 rgba(0, 0, 0, 0.2);
+		z-index: 1;
+	}
+
+	#modal_text {
+		/* z-index: 12; */
+	}
+
+	#modal_close {
+		/* z-index: 12; */
+	}
+
+	#modal_background {
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		margin: 0;
+		background-color: #0005;
+		z-index: -1;
+	}
+</style>
