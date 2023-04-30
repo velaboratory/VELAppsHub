962e4faa289dd1e8a227e4ecce62004f72b0d1c3
Anton Franzluebbers <anton@uga.edu>
2023-04-29 22:27:28 -0400

    add upload dashboard

--- AppServer/server.py
+++ AppServer/server.py
@@ -1,9 +1,15 @@
 from enum import Enum
-from fastapi import FastAPI, Request
+from typing import Annotated
+import aiofiles
+from fastapi import Depends, FastAPI, File, Form, Request, Response, UploadFile, status
+from fastapi.responses import FileResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.templating import Jinja2Templates
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
 import json
 import os
+import secrets
 
 app = FastAPI()
 
@@ -26,6 +32,11 @@ class Platform(str, Enum):
     win = "win"
     mac = "mac"
     linux = "linux"
+
+
+templates = Jinja2Templates(directory="templates")
+
+security = HTTPBasic()
 
 
 @app.get('/get_apps')
@@ -61,3 +72,63 @@ async def get_apps(request: Request, access_code: str, platform: Platform):
                         '\\', '/')
 
     return {'apps': apps}
+
+
+@app.get('/')
+async def main(request: Request, response: Response, credentials: HTTPBasicCredentials = Depends(security)):
+    if credentials.username == secrets.username and credentials.password == secrets.password:
+        with open('apps.json', 'r') as f:
+            apps = json.load(f)['apps']
+            return templates.TemplateResponse("index.html", {"request": request, "apps": apps})
+    else:
+        print('no creds')
+        response.status_code = 401
+        return
+
+
+@app.post("/upload")
+async def create_upload_file(
+    file: UploadFile = File(),
+    folder: str = Form(...),
+    version: str = Form(...),
+    platform: str = Form(...),
+):
+    print(folder)
+    f = os.path.join('static', folder, 'zips', platform)
+    os.makedirs(f, exist_ok=True)
+    async with aiofiles.open(os.path.join(f, f'{folder}_v{version}.zip'), 'wb') as out_file:
+        while content := await file.read(1024):  # async read chunk
+            await out_file.write(content)  # async write chunk
+    return RedirectResponse("/")
+
+
+@app.post("/create_app")
+async def create_app(
+    name: str = Form(...),
+    description: str = Form(...),
+    folder: str = Form(...),
+    accessible_by: str = Form(...),
+):
+    apps = []
+    with open('apps.json', 'r') as f:
+        apps = json.load(f)['apps']
+        a = {}
+        for app in apps:
+            if app['folder'] == folder:
+                apps.remove(app)
+                break
+        a['folder'] = folder
+        a['name'] = name
+        a['description'] = description
+        a['accessible_by'] = [a.strip() for a in accessible_by.split(',')]
+        apps.append(a)
+
+    with open('apps.json', 'w') as f:
+        json.dump({'apps': apps}, f, indent=4)
+
+    return RedirectResponse(url="/", status_code=status.HTTP_302_FOUND)
+
+
+@app.get("/favicon.ico")
+async def favicon():
+    return FileResponse('favicon.png')
